e707fcd3850a11894ff752a969804f30d85cdaea
zhaole <zhaole@act.buaa.edu.cn>
2019-02-11 21:20:56 +0800

    邮件那里变了

---
+++ src/main/java/com/zhaole/async/handler/LikeHandler.java
@@ -0,0 +1,46 @@
+package com.zhaole.async.handler;
+
+import com.zhaole.async.EventHandler;
+import com.zhaole.async.EventModel;
+import com.zhaole.async.EventType;
+import com.zhaole.model.Message;
+import com.zhaole.model.User;
+import com.zhaole.service.MessageService;
+import com.zhaole.service.UserService;
+import com.zhaole.util.WendaUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by nowcoder on 2016/7/30.
+ */
+@Component
+public class LikeHandler implements EventHandler {
+    @Autowired
+    MessageService messageService;
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public void doHandle(EventModel model) {
+        Message message = new Message();
+        message.setFromId(WendaUtil.SYSTEM_USERID);
+        message.setToId(model.getEntityOwnerId());
+        message.setCreatedDate(new Date());
+        User user = userService.getUser(model.getActorId());
+        message.setContent("用户" + user.getName()
+                + "赞了你的评论,http://127.0.0.1:8080/question/" + model.getExt("questionId"));
+
+        messageService.addMessage(message);
+    }
+
+    @Override
+    public List<EventType> getSupportEventTypes() {
+        return Arrays.asList(EventType.LIKE);
+    }
+}
---
+++ src/main/java/com/zhaole/controller/LikeController.java
@@ -0,0 +1,65 @@
+package com.zhaole.controller;
+
+import com.zhaole.async.EventModel;
+import com.zhaole.async.EventProducer;
+import com.zhaole.async.EventType;
+import com.zhaole.model.Comment;
+import com.zhaole.model.EntityType;
+import com.zhaole.model.HostHolder;
+import com.zhaole.service.CommentService;
+import com.zhaole.service.LikeService;
+import com.zhaole.service.MessageService;
+import com.zhaole.util.WendaUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by nowcoder on 2016/7/30.
+ */
+@Controller
+public class LikeController {
+    @Autowired
+    LikeService likeService;
+
+    @Autowired
+    HostHolder hostHolder;
+
+    @Autowired
+    CommentService commentService;
+
+    @Autowired
+    EventProducer eventProducer;
+
+    @RequestMapping(path = {"/like"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public String like(@RequestParam("commentId") int commentId) {
+        if (hostHolder.getUser() == null) {
+            return WendaUtil.getJSONString(999);
+        }
+
+        Comment comment = commentService.getCommentById(commentId);
+
+        eventProducer.fireEvent(new EventModel(EventType.LIKE)
+                .setActorId(hostHolder.getUser().getId()).setEntityId(commentId)
+                .setEntityType(EntityType.ENTITY_COMMENT).setEntityOwnerId(comment.getUserId())
+                .setExt("questionId", String.valueOf(comment.getEntityId())));
+
+        long likeCount = likeService.like(hostHolder.getUser().getId(), EntityType.ENTITY_COMMENT, commentId);
+        return WendaUtil.getJSONString(0, String.valueOf(likeCount));
+    }
+
+    @RequestMapping(path = {"/dislike"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public String dislike(@RequestParam("commentId") int commentId) {
+        if (hostHolder.getUser() == null) {
+            return WendaUtil.getJSONString(999);
+        }
+
+        long likeCount = likeService.disLike(hostHolder.getUser().getId(), EntityType.ENTITY_COMMENT, commentId);
+        return WendaUtil.getJSONString(0, String.valueOf(likeCount));
+    }
+}
