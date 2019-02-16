028ff844acb584c357e892a149683fb78fcbd970
zhaole <zhaole@act.buaa.edu.cn>
2019-02-16 15:53:45 +0800

    follow

--- src/main/java/com/zhaole/util/RedisKeyUtil.java
+++ src/main/java/com/zhaole/util/RedisKeyUtil.java
@@ -1,24 +1,49 @@
 package com.zhaole.util;
 
 /**
- * Created by nowcoder on 2016/7/30.
+ *
  */
-public class RedisKeyUtil {
+public class RedisKeyUtil
+{
     private static String SPLIT = ":";
-    private static String BIZ_LIKE = "LIKE";
-    private static String BIZ_DISLIKE = "DISLIKE";
-    private static String BIZ_EVENTQUEUE = "EVENT_QUEUE";
+    private static String LIKE = "LIKE";
+    private static String DISLIKE = "DISLIKE";
+    private static String EVENTQUEUE = "EVENT_QUEUE";
 
-    public static String getLikeKey(int entityType, int entityId) {
-        return BIZ_LIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
+    private static String FOLLOWER = "FOLLOWER";
+    private static String FOLLOWEE = "FOLLOWEE";
+    private static String TIMELINE = "TIMELINE";
+
+    //获取点赞的key
+    public static String getLikeKey(int entityType, int entityId)
+    {
+        return LIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
     }
 
-    public static String getDisLikeKey(int entityType, int entityId) {
-        return BIZ_DISLIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
+    public static String getDisLikeKey(int entityType, int entityId)
+    {
+        return DISLIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
     }
 
-    public static String getEventQueueKey() {
-        return BIZ_EVENTQUEUE;
+    public static String getEventQueueKey()
+    {
+        return EVENTQUEUE;
+    }
+    //获取每一个实体（问题或者是人）的粉丝对象的key
+    public static String getFollowerKey(int entityId, int entityType)
+    {
+        return FOLLOWER + SPLIT + String.valueOf(entityId) + SPLIT + String.valueOf(entityType);
     }
 
+    //获取关注的实体的key也就是某个人关注的某一类问题
+    public static String getFolloweeKey(int userId, int entityType)
+    {
+        return FOLLOWEE + SPLIT + String.valueOf(userId) + SPLIT + String.valueOf(entityType);
+    }
+
+    //获取TimeLine的key
+    public static String getTimelineKey(int userId)
+    {
+        return TIMELINE + SPLIT + String.valueOf(userId);
+    }
 }
