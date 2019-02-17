89d73ba0000a13687945ee378e9f4d89a09b7a52
zhaole <zhaole@act.buaa.edu.cn>
2019-02-17 21:24:04 +0800

    application

--- src/main/java/com/zhaole/WendaApplication.java
+++ src/main/java/com/zhaole/WendaApplication.java
@@ -2,13 +2,23 @@ package com.zhaole;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * created by zl on 2019/1/24 15:56
  */
 @SpringBootApplication
-public class WendaApplication {
-    public static void main(String[] args) {
+public class WendaApplication  extends SpringBootServletInitializer
+{
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+    {
+        return builder.sources(WendaApplication.class);
+    }
+
+    public static void main(String[] args)
+    {
         SpringApplication.run(WendaApplication.class, args);
     }
 }
