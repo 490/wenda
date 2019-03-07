1eda914a8eee1a56499524103560a3388774073f
zhaole <zhaole@act.buaa.edu.cn>
2019-03-07 15:13:11 +0800

    html druid数据库配置文件密码加密

--- src/main/java/com/zhaole/util/WendaWebConfiguration.java
+++ src/main/java/com/zhaole/util/WendaWebConfiguration.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * created by zl on 2019/1/25 8:59
